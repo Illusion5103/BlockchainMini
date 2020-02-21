21874d9be60c7360cd407e9eb5708fb924b87880
nshenk <nhshenk@gmail.com>
2020-02-20 23:03:39 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # miniBlockchainExample
+
+Simple example of a blockchain built in Python. Note that a full blockchain would be distributed; this is hosted completely on a single computer as the purpose is to demonstrate basic blockchain architecture.
---
+++ script.py
@@ -0,0 +1,18 @@
+from blockchain import Blockchain
+
+block_one_transactions = {"sender":"Guy", "receiver": "Pal", "amount":"1"}
+block_two_transactions = {"sender": "Buddy", "receiver":"Friend", "amount":"20000"}
+block_three_transactions = {"sender":"Pal", "receiver":"Guy", "amount":"75"}
+fake_transactions = {"sender": "Friend", "receiver":"Cole, Guy", "amount":"0.1"}
+
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+
+#Adding a fake transaction to test blockchain security
+#local_blockchain.chain[2].transactions = fake_transactions
+#local_blockchain.validate_chain()
